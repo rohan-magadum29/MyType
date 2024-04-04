e8b30d9019adb11d46c8a2567a63cba866e83219
rohan <rohanmagadum619@gmail.com>
2024-04-04 05:39:45 +0530

    Fixed the timer Selector UI

--- src/components/TimerSelector.js
+++ src/components/TimerSelector.js
@@ -1,37 +1,34 @@
-import { useState,useContext } from "react";
+import React, { useContext } from "react";
 import TimerContext from "../contexts/TimerContext";
 
 const TimerSelector = () => {
+  const { selectedTime, setSelectedTime } = useContext(TimerContext);
 
-  const {selectedTime,setSelectedTime} = useContext(TimerContext);
-  const handleTimeSelection = (event) => {
-    setSelectedTime(parseInt(event.target.value));
+  const handleTimeSelection = (time) => {
+    setSelectedTime(time);
     console.log(selectedTime);
   };
 
   return (
     <div className="timer-selector">
-      <input
-        type="radio"
-        value={15}
-        checked={selectedTime === 15}
-        onChange={handleTimeSelection}
-      />
-      15 sec
-      <input
-        type="radio"
-        value={30}
-        checked={selectedTime === 30}
-        onChange={handleTimeSelection}
-      />
-      30 sec
-      <input
-        type="radio"
-        value={60}
-        checked={selectedTime === 60}
-        onChange={handleTimeSelection}
-      />
-      60 sec
+      <button
+        className={selectedTime === 15 ? "selected" : "unselected"}
+        onClick={() => handleTimeSelection(15)}
+      >
+        15 sec
+      </button>
+      <button
+        className={selectedTime === 30 ? "selected" : "unselected"}
+        onClick={() => handleTimeSelection(30)}
+      >
+        30 sec
+      </button>
+      <button
+        className={selectedTime === 60 ? "selected" : "unselected"}
+        onClick={() => handleTimeSelection(60)}
+      >
+        60 sec
+      </button>
     </div>
   );
 };
